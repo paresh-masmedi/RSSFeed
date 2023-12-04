b2c5ff2e8d6ec7f58c09ea26f81590ff7602f6d4
paresh-masmedi <117361949+paresh-masmedi@users.noreply.github.com>
2023-12-04 12:32:24 +0530

    loading view added

--- RSS Feed/RSS Feed/All Views/RSSFeed/RSSFeedView.swift
+++ RSS Feed/RSS Feed/All Views/RSSFeed/RSSFeedView.swift
@@ -18,30 +18,37 @@ struct RSSFeedView: View {
     
     @ObservedObject var viewModel: RSSFeedViewModel
         
-    @State var arrStr = ["A", "B", "C"]
-
     var body: some View {
-//        LoadingView(isShowing: $viewModel.isFetchingData) {
-            NavigationView {
-                    List {
-                        ForEach(viewModel.feeds, id: \.self) { feed in
-                            NavigationLink {
-                                RSSFeedDetail(feed: feed)
-                                
-                            } label: {
-                                Text(feed.title)
-                            }
+        NavigationView {
+            ZStack {
+                List {
+                    ForEach(viewModel.feeds, id: \.self) { feed in
+                        NavigationLink {
+                            RSSFeedDetail(feed: feed)
+                            
+                        } label: {
+                            Text(feed.title)
                         }
                     }
-                    .navigationTitle("Medium RSS")
-                    .onAppear {
-                        print("RSSFeedView: onAppear")
-                        
-                        //API Call
-                        self.viewModel.getFeedData()
+                }
+                .navigationTitle("Medium RSS")
+                .onAppear {
+                    print("RSSFeedView: onAppear")
+                    
+                    //API Call
+                    self.viewModel.getFeedData()
+                }
+                
+                if viewModel.isFetchingData {
+                    VStack(alignment: .center) {
+                        ProgressView()
+                        .progressViewStyle(CircularProgressViewStyle(tint: .blue))
+                        .scaleEffect(2.0, anchor: .center)
                     }
+                }
             }
-//        }
+            
+        }
     }
 }
 
